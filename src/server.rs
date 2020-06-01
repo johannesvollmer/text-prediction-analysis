b32103d6e9be224c4f08ed92678f6c3f6fcbc067
johannesvollmer <johannes.boenki@t-online.de>
2020-06-02 00:30:48 +0200

    various improvements

--- src/server.rs
+++ src/server.rs
@@ -5,6 +5,7 @@
 // http://norvig.com/spell-correct.html -> http://norvig.com/big.txt
 
 use serde::{Deserialize, Serialize};
+use std::time::Instant as time;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Request {
@@ -18,18 +19,21 @@ pub struct Response {
 }
 
 /// Start a server that answers JSON suggestion requests with JSON suggestions.
-pub fn run <F: (Fn(Request) -> Response)> (suggest: F)
+pub fn run <F: (Fn(Request) -> Response)> (compute_suggestions: F)
     where F: Sync + Send + 'static
 {
     println!("starting the suggestion server on localhost:3000");
 
     rouille::start_server_with_pool("localhost:3000", Some(1), move |request| {
         let request: Request = serde_json::from_reader(request.data().unwrap()).unwrap();
+
         println!("received a prediction request: {:?}", request);
+        let start_time = time::now();
 
-        let answer = suggest(request);
-        println!("computed answer: {:#?}", answer);
+        let answer = compute_suggestions(request);
 
+        let duration = (time::now() - start_time).as_secs_f32();
+        println!("computed answer in {}s: {:#?}", duration, answer);
         rouille::Response::json(&answer)
     });
 }
